10b0a6db9250baf48b296fc2dcb495d09c8be343
Nathan Butler <iamnbutler@gmail.com>
2013-09-21 14:22:38 -0400

    Merge branch 'master' of https://github.com/Jtupiter/anGo
    Conflicts:
    	web/css/scss/style.scss
    	web/css/style.css
    	web/index.html

--- web/js/scripts-ck.js
+++ web/js/scripts-ck.js
@@ -1,2 +1,28 @@
 // Load scripts after page loads
-$(document).ready(function(){$(document).ready(function(){$(".tiles").click(function(){$(".tiles").toggleClass("closed");$(".item-info").toggleClass("open")});$(".item-info .close").click(function(){$(".tiles").toggleClass("closed");$(".item-info").toggleClass("open")})})});
+$(document).ready(function(){
+  $(document).ready(function(){
+      $('.tiles .image').click(function(){
+        $('.tiles').toggleClass('closed');
+        $('.item-info').toggleClass('open');
+      });
+
+      $('.item-info .close').click(function(){
+        $('.tiles').toggleClass('closed');
+        $('.item-info').toggleClass('open');
+      });
+    
+      $('.details .close').click(function(){
+        $(this).parents('li').addClass('foldIn');
+      });
+    });
+});
+
+$(document).ready(function(){
+  $(".optionbar div.icon").click(function(){
+    $(".select").show();
+  });
+<<<<<<< HEAD
+}); 
+=======
+}); 
+>>>>>>> eaea8f59183f06ad73a5169f6ab54fdcfcc95728
--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,7 +1,7 @@
 // Load scripts after page loads
 $(document).ready(function(){
   $(document).ready(function(){
-      $('.tiles').click(function(){
+      $('.tiles .image').click(function(){
         $('.tiles').toggleClass('closed');
         $('.item-info').toggleClass('open');
       });
@@ -10,5 +10,19 @@ $(document).ready(function(){
         $('.tiles').toggleClass('closed');
         $('.item-info').toggleClass('open');
       });
+    
+      $('.details .close').click(function(){
+        $(this).parents('li').addClass('foldIn');
+      });
     });
 });
+
+$(document).ready(function(){
+  $(".optionbar div.icon").click(function(){
+    $(".select").show();
+  });
+<<<<<<< HEAD
+}); 
+=======
+}); 
+>>>>>>> eaea8f59183f06ad73a5169f6ab54fdcfcc95728
