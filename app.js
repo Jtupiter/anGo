934ce758f05ca8c066b8e0bd3d777b39c60be564
Jordan <jordan.upiter@gmail.com>
2013-09-20 19:24:12 -0700

    Initial structure commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    mongojs = require('mongojs'),
+    app = express(),
+    server = require('http').createServer(app);
+
+server.listen(process.env.PORT || 5000);
+
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/web'));
+
+var mongoUrl = process.env.MONGOHQ_URL || 'mongodb://localhost';
+var db = mongojs.connect(mongoUrl);
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
